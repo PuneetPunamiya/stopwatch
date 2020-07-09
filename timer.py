9e69fcbda6847d979f2809f5e2c15637ec79fe3c
PuneetPunamiya <puneetpunamiya@gmail.com>
2020-07-09 17:05:18 +0530

    Stopwatch using tkinter

---
+++ timer.py
@@ -0,0 +1,22 @@
+import sys
+import time
+import tkinter as tk
+
+counter = 120
+def counter_label(label):
+    counter = 120
+    def count():
+        global counter
+        counter = counter - 1
+        label.config(text=str(counter))
+        label.after(1000, count)
+    count()
+
+root = tk.Tk()
+root.title("stopwatch")
+label = tk.Label(root, font = ("times ", 20, "bold"),  width = 10)
+label.pack()
+counter_label(label)
+button = tk.Button(root, text = "stop", width = 40, command = root.destroy)
+button.pack()
+root.mainloop()
