884590d40d1b8242ccb1bb6220c48bd6d396da0d
PuneetPunamiya <puneetpunamiya@gmail.com>
2020-07-10 22:27:51 +0530

    Add reset and stop

--- timer.py
+++ timer.py
@@ -3,25 +3,69 @@ import time
 import tkinter as tk
 
 counter = 120
-def counter_label(label):
+def count_label(label):
     counter = 120
     def count():
         global counter
-
         if counter == 0:
             return
-
-        counter = counter - 1
-        label.config(text=str(counter))
-        label.after(1000, count)
+        if run:
+            # Just beore starting
+            if counter == 120:
+                show = "Starting"
+            else:
+                show = str(counter)
+            label['text'] = show
+            #Increment the count after
+            #every 1 second
+            label.after(1000, count)
+            counter -= 1
 
     count()
 
-root = tk.Tk()
-root.title("stopwatch")
-label = tk.Label(root, font = ("times ", 20, "bold"),  width = 10)
-label.pack()
-counter_label(label)
-button = tk.Button(root, text = "stop", width = 40, command = root.destroy)
-button.pack()
-root.mainloop()
+# While Running
+def Start(mark):
+   global run
+   run = True
+   count_label(mark)
+   start['state'] = 'disabled'
+   stop['state'] = 'normal'
+   reset['state'] = 'normal'
+
+# While stopped
+def Stop():
+   global run
+   start['state'] = 'normal'
+   stop['state'] = 'disabled'
+   reset['state'] = 'normal'
+   run = False
+
+# For Reset
+def Reset(label):
+   global counter
+   counter = 120
+   if run == False:
+      reset['state'] = 'disabled'
+      label['text'] = 'Welcome'
+   else:
+      label['text'] = 'Start'
+      start['state'] = 'normal'
+
+if __name__=="__main__":
+    root = tk.Tk()
+    root.title("stopwatch")
+    root.minsize(width=300, height=200)
+
+    label = tk.Label(root, font = ("times ", 25, "bold"))
+    label.pack()
+
+    start = tk.Button(root, text='Start',width = 20, height = 1, font = "10", command=lambda: Start(label))
+    stop = tk.Button(root, text='Stop', width=20, height = 1, font = "10", state='disabled', command=Stop)
+    reset = tk.Button(root, text='Reset',width=20, height = 1, font = "10", state='disabled', command=lambda: Reset(label))
+
+    start.pack()
+    stop.pack()
+    reset.pack()
+
+
+    root.mainloop()
